f9b68137618dad522c3f7c1643c5f1111380cffc
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-31 12:33:49 -0700

    Created classes and assigned sub-classes

---
+++ Shopping List/View/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Enayatullah Naseri on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
