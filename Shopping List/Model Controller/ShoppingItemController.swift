6a7565173a95beeedaa8e2fe7df881a4b4963359
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-31 12:28:22 -0700

    Updated Storyboard and created Model and ModelControler

---
+++ Shopping List/Model Controller/ShoppingItemController.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Enayatullah Naseri on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+class ShoppingItemController {
+    
+    private(set) var shoppingItem: [ShoppingItem] = []
+    
+    func createShoppingItem() {
+        
+        // copy, pasted list of items
+        let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+    }
+}
---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Enayatullah Naseri on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    var name: String
+    var hasBeenAdded: Bool
+    
+    // initializer to assign default value to false
+    init(name: String, hasBeenAdded: Bool = false) {
+        self.name = name
+        self.hasBeenAdded = hasBeenAdded
+    }
+}
